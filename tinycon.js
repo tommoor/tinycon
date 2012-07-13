595eec2b0c48f648da79ac72fd43881a5aa53211
yoko <yoko@carbonfairy.org>
2012-07-13 18:05:50 +0900

    一度もアイコンが書き換えられていない時、resetメソッドは何もしないようにした

--- tinycon.js
+++ tinycon.js
@@ -42,15 +42,15 @@
 	
 	// private methods
 	var getFaviconTag = function(){
-		
+
 		var links = document.getElementsByTagName('link');
-		
+
 		for(var i=0, len=links.length; i < len; i++) {
 			if ((links[i].getAttribute('rel') || '').match(/\bicon\b/)) {
 				return links[i];
 			}
 		}
-		
+
 		return false;
 	};
 	
@@ -58,7 +58,7 @@
 	
 		var links = document.getElementsByTagName('link');
 		var head = document.getElementsByTagName('head')[0];
-		
+
 		for(var i=0, len=links.length; i < len; i++) {
 			var exists = (typeof(links[i]) !== 'undefined');
 			if (exists && links[i].getAttribute('rel') === 'icon') {
@@ -68,7 +68,7 @@
 	};
 	
 	var getCurrentFavicon = function(){
-		
+
 		if (!originalFavicon || !currentFavicon) {
 			var tag = getFaviconTag();
 			originalFavicon = currentFavicon = tag ? tag.getAttribute('href') : '/favicon.ico';
@@ -78,19 +78,19 @@
 	};
 	
 	var getCanvas = function (){
-		
+
 		if (!canvas) {
 			canvas = document.createElement("canvas");
 			canvas.width = 16;
 			canvas.height = 16;
 		}
-		
+
 		return canvas;
 	};
 	
 	var setFaviconTag = function(url){
 		removeFaviconTag();
-		
+
 		var link = document.createElement('link');
 		link.type = 'image/x-icon';
 		link.rel = 'icon';
@@ -108,39 +108,39 @@
 		if (!getCanvas().getContext || browser.ie || browser.safari || options.fallback === 'force') {
 			return updateTitle(num);
 		}
-		
+
 		var context = getCanvas().getContext("2d");
 		var colour = colour || '#000000';
 		var num = num || 0;
 		var src = getCurrentFavicon();
-		
+
 		faviconImage = new Image();
 		faviconImage.onload = function() {
-			
+
 			// clear canvas  
 			context.clearRect(0, 0, 16, 16);
 
 			// draw original favicon
 			context.drawImage(faviconImage, 0, 0, faviconImage.width, faviconImage.height, 0, 0, 16, 16);
-			
+
 			// draw bubble over the top
 			if (num > 0) drawBubble(context, num, colour);
-			
+
 			// refresh tag in page
 			refreshFavicon();
 		};
-		
+
 		// allow cross origin resource requests if the image is not a data:uri
 		// as detailed here: https://github.com/mrdoob/three.js/issues/1305
 		if (!src.match(/^data/)) {
 			faviconImage.crossOrigin = 'anonymous';
 		}
-		
+
 		faviconImage.src = src;
 	};
 	
 	var updateTitle = function(num) {
-		
+
 		if (options.fallback) {
 			if (num > 0) {
 				document.title = '('+num+') ' + originalTitle;
@@ -151,7 +151,7 @@
 	};
 	
 	var drawBubble = function(context, num, colour) {
-		
+
 		// bubble needs to be larger for double digits
 		var len = (num+"").length-1;
 		var width = options.width + (6*len);
@@ -163,34 +163,34 @@
 		context.fillStyle = options.background;
 		context.strokeStyle = options.background;
 		context.lineWidth = 1;
-		
+
 		// bubble
 		context.fillRect(w,h,width-1,options.height);
-		
+
 		// rounded left
 		context.beginPath();
 		context.moveTo(w-0.5,h+1);
 		context.lineTo(w-0.5,15);
 		context.stroke();
-		
+
 		// rounded right
 		context.beginPath();
 		context.moveTo(15.5,h+1);
 		context.lineTo(15.5,15);
 		context.stroke();
-		
+
 		// bottom shadow
 		context.beginPath();
 		context.strokeStyle = "rgba(0,0,0,0.3)";
 		context.moveTo(w,16);
 		context.lineTo(15,16);
 		context.stroke();
-		
+
 		// number
 		context.fillStyle = options.colour;
 		context.textAlign = "right";
 		context.textBaseline = "top";
-		
+
 		// unfortunately webkit/mozilla are a pixel different in text positioning
 		context.fillText(num, 15, browser.mozilla ? 7 : 6);  
 	};
@@ -198,7 +198,7 @@
 	var refreshFavicon = function(){
 		// check support
 		if (!getCanvas().getContext) return;
-		
+
 		setFaviconTag(getCanvas().toDataURL());
 	};
 	
@@ -206,7 +206,7 @@
 	// public methods
 	Tinycon.setOptions = function(custom){
 		options = {};
-		
+
 		for(var key in defaults){
 			options[key] = custom.hasOwnProperty(key) ? custom[key] : defaults[key];
 		}
@@ -220,16 +220,16 @@
 	};
 	
 	Tinycon.setBubble = function(num, colour){
-		
+
 		// validate
 		if(isNaN(parseFloat(num)) || !isFinite(num)) return log('Bubble must be a number');
-		
+
 		drawFavicon(num, colour);
 		return this;
 	};
 	
 	Tinycon.reset = function(){
-		Tinycon.setImage(originalFavicon);
+		originalFavicon && Tinycon.setImage(originalFavicon);
 	};
 	
 	Tinycon.setOptions(defaults);
